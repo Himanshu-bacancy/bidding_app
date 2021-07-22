65c8f0e93867aa5690dfc9e5f6d2de6d361922f5
Brijesh <brijesh.ramavat@bacancy.com>
2021-07-22 17:27:44 +0530

    Add card validation method

--- application/controllers/rest/Card.php
+++ application/controllers/rest/Card.php
@@ -46,18 +46,30 @@ class Card extends API_Controller {
 
         $user_id = $this->post('user_id');
         $card_holder_name = $this->post('card_holder_name');
-        $card_number = $this->post('card_number');
+        $card_number = str_replace(' ', '', $this->post('card_number'));
         $expiry_date = $this->post('expiry_date');
         $address_id = $this->post('address_id');
-        
-        $is_record_already_exists = $this->db->select('id')->from('bs_card')->where('user_id', $user_id)->where('card_number', $card_number)->get()->num_rows();
-        $success_message = "Card already added";
-        if(!$is_record_already_exists) {
-            $this->db->insert('bs_card', ['user_id' => $user_id, 'card_holder_name' => $card_holder_name, 'card_number' => $card_number, 'expiry_date' => $expiry_date, 'address_id' => $address_id, 'created_date' => date('Y-m-d H:i:s')]);
-            $success_message = "Card added successfully";
+        $expiry_arr = explode('/', $expiry_date);
+        $expiry_arr[1] = '20'.$expiry_arr[1];
+        $expiry_date = implode('/',$expiry_arr);
+        $validate_date  = $this->validate_expirydate($expiry_date);
+        if($validate_date) {
+            $card_type = $this->validate_customer_card($card_number);
+            if($card_type != '') {
+                $is_record_already_exists = $this->db->select('id')->from('bs_card')->where('user_id', $user_id)->where('card_number', $card_number)->get()->num_rows();
+                $success_message = "Card already added";
+                if(!$is_record_already_exists) {
+                    $this->db->insert('bs_card', ['user_id' => $user_id, 'card_holder_name' => $card_holder_name, 'card_number' => $card_number, 'expiry_date' => $expiry_date, 'address_id' => $address_id, 'created_date' => date('Y-m-d H:i:s')]);
+                    $success_message = "Card added successfully";
+                }
+                $this->success_response( $success_message);
+            } else {
+                $this->error_response("Invalid card number");
+            }
+        } else {
+            $this->error_response("Card is already expired");
         }
     
-        $this->success_response( $success_message);
     }
     
     public function remove_card_post() {
@@ -101,6 +113,97 @@ class Card extends API_Controller {
         $obj = $this->db->from('bs_card')->where('user_id', $user_id)->get()->result();
         
         $this->response( ['status' => "success", 'cards' => $obj]);
+    }
+    
+    
+    public function validate_expirydate($expiry_date = NULL){
+
+        $return = FALSE;
+
+        $expiry_date_array = explode('/',$expiry_date);
+
+        if (count($expiry_date_array) == 2) {
+
+            if (checkdate($expiry_date_array[0], '01' , $expiry_date_array[1])) {
+
+                $expiry_date_obj = DateTime::createFromFormat('d/m/Y H:i:s', "01/" . $expiry_date_array[0] . "/" .  $expiry_date_array[1]." 00:00:00");
+
+                $expiry_date_obj = new DateTime($expiry_date_obj->format("Y-m-t"));
+
+                $my_date = date('d/m/Y');
+
+                $today = DateTime::createFromFormat('d/m/Y H:i:s', $my_date ." 00:00:00");
+
+                if($expiry_date_obj >= $today){
+
+                    $return = TRUE;
+
+                }
+
+            }
+
+        }
+
+        return $return;
+
+    }
+
+    
+
+    function validate_customer_card($card_number = NULL){
+
+        if (isset($card_number) && $card_number != ''){
+
+            global $type;
+
+            $cardtype = array(
+
+                "visa"       => "/^4[0-9]{12}(?:[0-9]{3})?$/",
+
+                "mastercard" => "/^5[1-5][0-9]{14}$/",
+
+                "amex"       => "/^3[47][0-9]{13}$/",
+
+                "jcb"        => "/^(?:2131|1800|35\d{3})\d{11}$/",
+
+                "dinnerclub" => "/^3(?:0[0-5]|[68][0-9])[0-9]{11}$/",
+
+                "discover"   => "/^6(?:011|5[0-9]{2})[0-9]{12}$/",
+
+            );
+
+            if (preg_match($cardtype['visa'],$card_number)) {
+
+                return 'visa';
+
+            } else if (preg_match($cardtype['mastercard'],$card_number)) {
+
+                return 'mastercard';
+
+            } else if (preg_match($cardtype['dinnerclub'],$card_number)) {
+
+                return 'dinnerclub';
+
+            } else if (preg_match($cardtype['jcb'],$card_number)) {
+
+                return 'jcb';
+
+            } else if (preg_match($cardtype['amex'],$card_number)) {
+
+                return 'amex';
+
+            } else if (preg_match($cardtype['discover'],$card_number)) {
+
+                return 'discover';
+
+            } else {
+
+                return '';
+
+            }
+
+        }
+
     }
 
 }
