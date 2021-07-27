14455545d782d236a1b92e4758a5f48e1cbd1bfb
Shraddha <shraddha.vaghela@bacancy.com>
2021-07-27 16:18:22 +0530

    - Offer Listing api changes ( Add Extra params ) - Convert Exchange data ( key change as per api )

--- application/libraries/PS_Adapter.php
+++ application/libraries/PS_Adapter.php
@@ -1030,7 +1030,7 @@ class PS_Adapter {
 	 * @param      <type>  $obj    The object
 	 */
 	function convert_chathistory( &$obj , $is_exchange_detail_req = true )
-	{
+	{	
 		if ( is_array( $obj )) {
 			for ($i=0; $i < count($obj) ; $i++) { 
 
@@ -1071,14 +1071,15 @@ class PS_Adapter {
 				if ( isset( $obj[$i]->operation_type ) && $obj[$i]->operation_type == EXCHANGE && isset($obj[$i]->id) ) {
 					$tmp_exchange_ids = $this->CI->ExchangeChatHistory->get_all_in_exchange_chat_item( array('chat_id' => $obj[$i]->id) );
 
-					foreach($tmp_exchange_ids as $offered){
-						$obj[$i]->offered_item_id[] = $offered->offered_item_id;
-					}
+					// foreach($tmp_exchange_ids as $offered){
+					// 	$obj[$i]->offered_item_id[] = $offered->offered_item_id;
+					// }
 					$this->convert_exchange_chat_history( $tmp_exchange_ids );
-					$obj[$i]->exchange_chat_detail = $tmp_exchange_ids;
+					// $obj[$i]->exchange_chat_detail = $tmp_exchange_ids;
 					
-					foreach($obj[$i]->exchange_chat_detail as $exchange_data){
-						$obj[$i]->offered_item_detail[] = $exchange_data->offered_item_detail;
+					foreach($tmp_exchange_ids as $exchange_data){
+						$obj[$i]->exchange_item_id[] = $exchange_data->offered_item_id;
+						$obj[$i]->exchange_item_detail[] = $exchange_data->offered_item_detail;
 					}
 				} else{
 					// IF OFFERED ITEM ID EXIST THEN GET DETAILS OF ITEM
@@ -1144,17 +1145,18 @@ class PS_Adapter {
 				
 				$tmp_exchange_ids = $this->CI->ExchangeChatHistory->get_all_in_exchange_chat_item( array('chat_id' => $obj->id) );
 
-				foreach($tmp_exchange_ids as $offered){
-					$obj->offered_item_id[] = $offered->offered_item_id;
-				}
+				// foreach($tmp_exchange_ids as $offered){
+				// 	$obj->offered_item_id[] = $offered->offered_item_id;
+				// }
 
 				if($is_exchange_detail_req == true){
 					$this->convert_exchange_chat_history( $tmp_exchange_ids );
 				}
-				$obj->exchange_chat_detail = $tmp_exchange_ids;
+				// $obj->exchange_chat_detail = $tmp_exchange_ids;
 				
-				foreach($obj->exchange_chat_detail as $exchange_data){
-					$obj->offered_item_detail[] = $exchange_data->offered_item_detail;
+				foreach($tmp_exchange_ids as $exchange_data){
+					$obj->exchange_item_id[] = $offered->offered_item_id;
+					$obj->exchange_item_detail[] = $exchange_data->offered_item_detail;
 				}
 			} else{
 				// IF OFFERED ITEM ID EXIST THEN GET DETAILS OF ITEM
