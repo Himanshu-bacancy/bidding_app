48bd556a28b9a08126b8a495c4082644275aaba8
Himanshu Sharma <himanshu.sharam@bacancy.com>
2021-07-29 15:31:35 +0530

    changes in similarcriteria api

--- application/controllers/rest/Appinfo.php
+++ application/controllers/rest/Appinfo.php
@@ -34,6 +34,10 @@ class Appinfo extends API_Controller
             'requireAuthorization' => true,
         ]);
 		$data = $this->db->get('bs_similar_criterias')->result();
+		foreach($data as $key=>$similarcriteria){
+			$data[$key]->default_icon = $this->get_default_photo( $similarcriteria->id, 'similarcriteria_icon' );
+		}
+		echo '<pre>'; print_r($data); die;
 		//$rows = '[{"id":1,"title":"Brand"},{"id":2,"title":"Size"},{"id":3,"title":"Condition"},{"id":4,"title":"Color"}]';
 
 		//$objArray = json_decode($data);
