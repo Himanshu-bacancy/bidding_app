537d1fa8b3b575d464d3b48a44272da09a4abd6d
Brijesh <brijesh.ramavat@bacancy.com>
2021-08-17 13:09:42 +0530

    Add conflict changes

--- application/controllers/rest/Payments.php
+++ application/controllers/rest/Payments.php
@@ -115,7 +115,7 @@ class Payments extends API_Controller {
                 $record_id = $this->db->insert_id();
                 if (isset($response->id)) { 
                     $this->db->where('id', $record_id)->update(['status' => 'initiate', 'transaction_id' => $response->id]);
-                    $this->response(['status' => "success", 'order_status' => 'success', 'intent_id' => $response->id, 'record_id' => $record_id, 'client_secret' => $response->client_secret]);
+                    $this->response(['status' => "success", 'order_status' => 'success', 'intent_id' => $response->id, 'record_id' => $record_id, 'client_secret' => $response->client_secret, 'response' => $response]);
                 } else {
                     $this->db->where('id', $record_id)->update(['status' => 'fail']);
                     $this->error_response(get_msg('stripe_transaction_failed'));
